216a0d4772b7433e3415217d605b0d070f72e3a9
limjung99 <limjung99@naver.com>
2024-06-06 20:34:36 +0900

    Feat : security config

--- src/main/java/com/springsecurity/controller/ExerciseController.java
+++ src/main/java/com/springsecurity/controller/ExerciseController.java
@@ -1,6 +1,7 @@
 package com.springsecurity.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExerciseController {
 
     @PostMapping("/login")
+    @PreAuthorize("hasRole(ROLE_USER)")
     public ResponseEntity<Object> login(){
-
+        return ResponseEntity.ok().body("login success");
     }
 
 }
